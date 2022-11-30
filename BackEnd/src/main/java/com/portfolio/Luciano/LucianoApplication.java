40cd1d8158b5f667fee7617a0ae2ce9802abfa3b
Luciano Coronel <86379664+lucho9595@users.noreply.github.com>
2022-11-30 15:02:43 -0300

    add carpet

---
+++ BackEnd/src/main/java/com/portfolio/Luciano/LucianoApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Luciano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucianoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucianoApplication.class, args);
+	}
+
+}
