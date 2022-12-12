c517a88cf1cbbd3a4dce08fb2c6fe75cedaa97c5
Luciano Coronel <86379664+lucho9595@users.noreply.github.com>
2022-12-12 16:45:06 -0300

    Terminamos El JWT en el back!

--- BackEnd/src/main/java/com/portfolio/Luciano/Security/MainSecurity.java
+++ BackEnd/src/main/java/com/portfolio/Luciano/Security/MainSecurity.java
@@ -4,10 +4,70 @@
  */
 package com.portfolio.Luciano.Security;
 
-/**
- *
- * @author Luciano
- */
-public class MainSecurity {
-    
+import com.portfolio.Luciano.Security.Service.UserDetailsImplement;
+import com.portfolio.Luciano.Security.jwt.JwtEntryPoint;
+import com.portfolio.Luciano.Security.jwt.JwtTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+//lo que hace esta notation es habilitar la opcion
+//que los metodos que nosotros digamos que tienen que estar logeados
+//van a estar bloqueados por esta opcion, tenes que estar logeado si o si
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class MainSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsImplement userDetailsServiceImplement;
+    @Autowired
+    JwtEntryPoint jwtEntryPoint;
+
+    @Bean
+    public JwtTokenFilter jwtTokenFilter() {
+        return new JwtTokenFilter();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsServiceImplement).passwordEncoder(passwordEncoder());
+    }
+
 }
