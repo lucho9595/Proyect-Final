40447c321866ea4d41798fa4651cf9a80e6c25b7
lucho9595 <lucianocoronel48@gmail.com>
2022-12-13 13:28:35 -0300

    auth finish, finished service!

--- FrontEnd/src/app/service/auth.service.ts
+++ FrontEnd/src/app/service/auth.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtDto } from '../model/jwt-dto';
+import { Observable } from 'rxjs';
+import { LoginUsuario } from '../model/login-usuario';
+import { NuevoUsuario } from '../model/nuevo-usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  authURL = 'http://localhost:8080/auth/';
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
+  public nuevo(nuevoUsuario: NuevoUsuario): Observable<any> {
+    return this.httpClient.post<any>(this.authURL + 'nuevo', nuevoUsuario);
+  }
+
+  public login(loginUsuario: LoginUsuario): Observable<JwtDto> {
+    return this.httpClient.post<JwtDto>(this.authURL + 'login', loginUsuario);
+  }
 }
